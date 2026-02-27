766365dd689153bbf6ea4b7ac2e93b44cc422f20
lasyasunkavilli408 <lasyasunkavilli2006@gmail.com>
2026-02-27 12:43:55 +0530

    Implement string permutations generator

---
+++ permutations of a given string.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class StringPermutations {
+    static void permute(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String left = str.substring(0, i);
+            String right = str.substring(i + 1);
+            permute(left + right, ans + ch);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String s = sc.nextLine();
+        System.out.println("Permutations:");
+        permute(s, "");
+    }
+}
